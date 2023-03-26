895b60e03e64e441f3a7a06455bc8d28e920c9e2
magicse <magicse@users.noreply.github.com>
2023-03-26 14:59:16 +0300

    Create Utility.cpp

---
+++ src/Utility.cpp
@@ -0,0 +1,19 @@
+#include "Utility.h"
+#include <cmath>
+#include <functional>
+
+using namespace std;
+
+namespace Utility {
+    double blackman_harris(int n, int samples){
+
+        const float a0 = 0.35875;
+        const float a1 = 0.48829;
+        const float a2 = 0.14128;
+        const float a3 = 0.01168;
+
+        const float arg = 2 * M_PI * n / (samples - 1);
+
+        return a0 - a1 * cos(arg) + a2 * cos(2 * arg) - a3 * cos( 3 * arg);
+    }
+}
