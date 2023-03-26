14da51d7730da7c10e00c02a15f47e2ba42ade14
magicse <magicse@users.noreply.github.com>
2023-03-26 15:00:20 +0300

    Update Utility.cpp

--- src/Utility.cpp
+++ src/Utility.cpp
@@ -5,6 +5,12 @@
 using namespace std;
 
 namespace Utility {
+
+    double hann_function(int n, int samples){
+        return 0.5 * (1.0 - cos((2.0 * M_PI * n)/(samples - 1.0)));
+
+    }
+
     double blackman_harris(int n, int samples){
 
         const float a0 = 0.35875;
