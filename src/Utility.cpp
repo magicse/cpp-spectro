3bc23d87e7ce4e99fd5b9cb2dfbc4caa9539440c
magicse <magicse@users.noreply.github.com>
2023-03-30 19:34:58 +0300

    Update Utility.cpp

--- src/Utility.cpp
+++ src/Utility.cpp
@@ -49,7 +49,7 @@ namespace Utility {
         return a0 - a1 * cos(arg) + a2 * cos(2 * arg) - a3 * cos( 3 * arg);
     }
     
-    //Calc magnitude from complex number
+    //Calc magnitude for complex number
     double mag(complex_d& c){
         return sqrt(c.real() * c.real() + c.imag() * c.imag());
     }
